cce477858e6c1ee3696eaa5f0d4df64eee25f9b2
Oscar <au.oscar@hotmail.com>
2019-11-13 14:18:51 -0800

    pls

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, "build", '../client/build/index.html'));
 })
 
 // *************************************************************** //
